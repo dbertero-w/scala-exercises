fddb924d61ec863b8b23e6174453c209bbda4212
Dario Bertero <dbertero.w@gmail.com>
2021-09-11 11:35:36 +0100

    Carry on chapter 3 Chiusano

--- src/chapter_3_1.scala
+++ src/chapter_3_1.scala
@@ -1,3 +1,5 @@
+import sun.font.TrueTypeFont
+
 object chapter_3_1 {
   def tail[T](xs: List[T]): List[T] = {
     xs match {
@@ -91,8 +93,97 @@ object chapter_3_1 {
     })
   }
 
+  def myMap[A, B](as: List[A])(f: A => B): List[B] = {
+    as match {
+      case Nil => Nil
+      case x :: xs => f(x) :: myMap(xs)(f)
+    }
+  }
+
+  def myFilter[A](as: List[A])(f: A => Boolean): List[A] = {
+    myFlatMap(as)((x: A) => {
+      if (f(x))
+        List(x)
+      else
+        Nil
+    })
+    //    as match {
+//      case Nil => Nil
+//      case x :: xs => {
+//        if (f(x))
+//          x :: myFilter(xs)(f)
+//        else
+//          myFilter(xs)(f)
+//      }
+//    }
+  }
+
+  def myFlatMap[A, B](as: List[A])(f: A => List[B]): List[B] = {
+    as match {
+      case Nil => Nil
+      case x :: xs => f(x) ++ myFlatMap(xs)(f)
+    }
+  }
+
+  def addOne(xs: List[Int]): List[Int] = {
+    myMap(xs)((a: Int) => a + 1)
+  }
+
+  def stringify(xs: List[Double]): List[String] = {
+    myMap(xs)((a: Double) => a.toString())
+  }
+
+  def zipList[A](as: List[A], bs: List[A]): List[List[A]] = {
+    as match {
+      case Nil => Nil
+      case x :: xs => {
+        bs match {
+          case Nil => Nil
+          case y :: ys => List(x, y) :: zipList(xs, ys)
+        }
+      }
+    }
+  }
+
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = {
+    def checkSubsequence(as: List[A], bs: List[A]): Boolean = {
+      as match {
+        case Nil => {
+          bs match {
+            case Nil => true
+            case _ => false
+          }
+        }
+        case x :: xs => {
+          bs match {
+            case Nil => true
+            case y :: ys => {
+              if (x == y)
+                checkSubsequence(xs, ys)
+              else
+                false
+            }
+          }
+        }
+      }
+    }
+
+    sup match {
+      case Nil => {
+        sub match {
+          case Nil => true
+          case ys => false
+        }
+      }
+      case x :: xs => {
+        checkSubsequence(x :: xs, sub) || hasSubsequence(xs, sub)
+        }
+      }
+    }
+
   def main(args: Array[String]): Unit = {
     val testList = List(1, 2, 3, 4, 5)
+    val testDouble = List(1.1, 2.3, 3.7, 4.9, 5.0)
     val emptyList = List()
     val singletonList = List(0)
 
@@ -112,5 +203,14 @@ object chapter_3_1 {
     println(newFoldLeft(testList, Nil)((as: List[Int], a) => a :: as))
     println(append(testList, -12))
     println(concatenate(List(testList, testList, testList)))
+    println(addOne(testList))
+    println(stringify(testDouble))
+    println(myFilter(testList)((x: Int) => x > 3))
+    println(myFlatMap(testList)((x: Int) => List(x, x, x)))
+    println(zipList(testList, testList))
+    println(hasSubsequence(testList, List(4, 5)))
+    println(hasSubsequence(testList, List(1, 3)))
+    println(hasSubsequence(testList, List(1, 2, 3, 4)))
+
   }
 }
